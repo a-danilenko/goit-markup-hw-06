2da8283e1dfa24e6a5b53ab9e76ff83480d19957
a-danilenko <adanilenko7@gmail.com>
2020-07-13 21:03:25 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -2,6 +2,7 @@
     const refs = {
       openModalBtn: document.querySelector('[data-modal-open]'),
       closeModalBtn: document.querySelector('[data-modal-close]'),
+      body: document.querySelector('[data-body]'),
       modal: document.querySelector('[data-modal]'),
     };
   
@@ -10,5 +11,6 @@
   
     function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
+      refs.body.classList.toggle('overflow');
     }
   })();
